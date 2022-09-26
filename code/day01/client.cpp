669f5d336687b8bea46c767e758fd5fb1afb2fde
Yap Miracle <2049829115@qq.com>
2022-09-26 18:22:18 +0800

    将之前学的Socket编程写进去了

--- code/day01/client.cpp
+++ code/day01/client.cpp
@@ -1,27 +1,62 @@
 /*
- * @Author: your name
- * @Date: 2022-01-04 20:03:45
- * @LastEditTime: 2022-01-05 19:08:58
- * @LastEditors: your name
+ * @Author: hzy
+ * @Date: 2022-09-26 17:21
+ * @LastEditTime: 2022-09-26 17:
+ * @LastEditors: hzy
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \30dayMakeCppServer\code\day01\client.cpp
  */
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <iostream>
+#include <unistd.h>
+#include <netdb.h>
 
-int main() {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
+#define handle_error(msg)\
+    do{perror(msg); exit(EXIT_FAILURE);}while(0)
+int main(int argc, char* argv[]) {
+    if(argc!=3){
+        std::cout<<"Usage: ./client port\n";
+        std::cout<<"Example: ./client 5005\n";
+        return -1;
+    }
+    struct hostent *h;
+    if((h=gethostbyname(argv[1]))==0){
+        handle_error("Get host by name failed!!");
+        return -1;
+    }
+    int serv_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(serv_fd==-1){
+        handle_error("Socket failed!!!");
+        return -1;
+    }
     struct sockaddr_in serv_addr;
     bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(8888);
 
-    //bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)); 客户端不进行bind操作
-
-    connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));    
-    
+    serv_addr.sin_port = htons(atoi(argv[2]));
+    memcpy(&serv_addr.sin_addr, h->h_addr, h->h_length);
+    if(connect(serv_fd, (sockaddr*)&serv_addr, sizeof(serv_addr))!=0){
+        handle_error("Connect failed!!!");
+        return -1;
+    }
+    char buffer[1024];
+    for(int i=0; i<10; i++){
+        sprintf(buffer, "You are a pig%d", i);
+        if(send(serv_fd, buffer, sizeof(buffer), 0)<=0){
+            handle_error("Send failed!!!");
+            break;
+        }
+        printf("Send: %s\n", buffer);
+        bzero(buffer, sizeof(buffer));
+//        sprintf(buffer, "O")
+        if(recv(serv_fd, buffer, sizeof(buffer), 0)<=0){
+            handle_error("Recv failed!!!");
+            break;
+        }
+        printf("Recv: %s\n", buffer);
+    }
+    close(serv_fd);
     return 0;
 }
--- code/day01/server.cpp
+++ code/day01/server.cpp
@@ -2,26 +2,52 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <cstdlib>
+#include <unistd.h>
 
-int main() {
+#define handle_error(msg)\
+    do{perror(msg); exit(EXIT_FAILURE);}while(0)
+int main(int argc, char* argv[]) {
+    if(argc!=2){
+        printf("Example: ./server 5005");
+        return -1;
+    }
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in serv_addr;
     bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(8888);
-
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+    bzero(serv_addr.sin_zero, 8);
     bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
 
     listen(sockfd, SOMAXCONN);
-    
+
     struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_len = sizeof(clnt_addr);
     bzero(&clnt_addr, sizeof(clnt_addr));
 
     int clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len);
 
-    printf("new client fd %d! IP: %s Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+//    printf("new client fd %d! IP: %s Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+    char buffer[1024];
+    while(true){
+        bzero(buffer, sizeof(buffer));
+        if(recv(clnt_sockfd, buffer, sizeof(buffer), 0)<=0){
+            handle_error("Recv failed!!!");
+            break;
+        }
+        printf("Recv: %s\n", buffer);
+        sprintf(buffer, "Ip is %s\n", inet_ntoa(clnt_addr.sin_addr));
+        if(send(clnt_sockfd, buffer, sizeof(buffer), 0)<=0){
+            handle_error("Send failed");
+            break;
+        }
+        printf("Send: %s\n", buffer);
+        
+    }
+    close(clnt_sockfd);
+    close(sockfd);
     return 0;
 }
