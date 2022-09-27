216319bdefae18d929abf51304abf00b1b368fe9
Yap Miracle <2049829115@qq.com>
2022-09-27 19:36:57 +0800

    客户端输入字符串服务端输出

--- code/day02/client.cpp
+++ code/day02/client.cpp
@@ -1,44 +1,75 @@
-#include <iostream>
+/*
+ * @Author: hzy
+ * @Date: 2022-09-27 18:00
+ * @LastEditTime: 2022-09-27 19.35
+ * @LastEditors: hzy
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \30dayMakeCppServer\code\day01\client.cpp
+ */
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <iostream>
 #include <unistd.h>
-#include "util.h"
+#include <netdb.h>
 
-
-int main() {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    errif(sockfd == -1, "socket create error");
-
+#define handle_error(msg)\
+    do{perror(msg); exit(EXIT_FAILURE);}while(0)
+#define error_if(condition, msg)\
+    do{\
+        if(condition){\
+            perror(msg);\
+            exit(EXIT_FAILURE);\
+        }\
+    }while(0)
+int main(int argc, char* argv[]) {
+    if(argc!=3){
+        std::cout<<"Usage: ./client port\n";
+        std::cout<<"Example: ./client 5005\n";
+        return -1;
+    }
+    struct hostent *h;
+    if((h=gethostbyname(argv[1]))==0){
+        handle_error("Get host by name failed!!");
+        return -1;
+    }
+    int serv_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(serv_fd==-1){
+        handle_error("Socket failed!!!");
+        return -1;
+    }
     struct sockaddr_in serv_addr;
     bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(8888);
 
-    errif(connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket connect error");
-    
+    serv_addr.sin_port = htons(atoi(argv[2]));
+    memcpy(&serv_addr.sin_addr, h->h_addr, h->h_length);
+    if(connect(serv_fd, (sockaddr*)&serv_addr, sizeof(serv_addr))!=0){
+        handle_error("Connect failed!!!");
+        return -1;
+    }
+    char buffer[1024];
     while(true){
-        char buf[1024];
-        bzero(&buf, sizeof(buf));
-        scanf("%s", buf);
-        ssize_t write_bytes = write(sockfd, buf, sizeof(buf));
-        if(write_bytes == -1){
+        bzero(buffer, sizeof(buffer));
+        scanf("%s", buffer);
+        ssize_t w = write(serv_fd, buffer, sizeof(buffer));
+        if(w==-1){
             printf("socket already disconnected, can't write any more!\n");
             break;
         }
-        bzero(&buf, sizeof(buf));
-        ssize_t read_bytes = read(sockfd, buf, sizeof(buf));
-        if(read_bytes > 0){
-            printf("message from server: %s\n", buf);
-        }else if(read_bytes == 0){
+        bzero(buffer, sizeof(buffer));
+        ssize_t r = read(serv_fd, buffer, sizeof(buffer));
+
+        if(r>=0){
+            printf("Message from server: %s\n", buffer);
+        }else if(r==0){
             printf("server socket disconnected!\n");
             break;
-        }else if(read_bytes == -1){
-            close(sockfd);
-            errif(true, "socket read error");
+        }else{
+            close(serv_fd);
+            error_if(true, "socket read error");
         }
     }
-    close(sockfd);
+    close(serv_fd);
     return 0;
 }
--- code/day02/server.cpp
+++ code/day02/server.cpp
@@ -2,47 +2,76 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <cstdlib>
 #include <unistd.h>
-#include "util.h"
 
-int main() {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    errif(sockfd == -1, "socket create error");
+#define handle_error(msg)\
+    do{perror(msg); exit(EXIT_FAILURE);}while(0)
+
+#define error_if(condition, msg)\
+    do{\
+    if(condition){\
+        perror(msg);\
+        exit(EXIT_FAILURE);\
+    }\
+}while(0)
+int main(int argc, char* argv[]) {
+    if(argc!=2){
+        printf("Example: ./server 5005");
+        return -1;
+    }
+    int sockfd;
+    error_if((sockfd= socket(AF_INET, SOCK_STREAM, 0))==-1, "Listen Socket created failed");
 
     struct sockaddr_in serv_addr;
     bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(8888);
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+    bzero(serv_addr.sin_zero, 8);
+    bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
 
-    errif(bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket bind error");
+    error_if(listen(sockfd, SOMAXCONN)==-1, "Listen failed!!!");
 
-    errif(listen(sockfd, SOMAXCONN) == -1, "socket listen error");
-    
     struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_len = sizeof(clnt_addr);
     bzero(&clnt_addr, sizeof(clnt_addr));
 
-    int clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len);
-    errif(clnt_sockfd == -1, "socket accept error");
+    int clnt_sockfd;
+    error_if((clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len))==-1,"Accept failed");
 
-    printf("new client fd %d! IP: %s Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
-    while (true) {
-        char buf[1024];
-        bzero(&buf, sizeof(buf));
-        ssize_t read_bytes = read(clnt_sockfd, buf, sizeof(buf));
+//    printf("new client fd %d! IP: %s Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+    char buffer[1024];
+    while(true){
+        bzero(buffer, sizeof(buffer));
+        ssize_t read_bytes = read(clnt_sockfd, buffer, sizeof(buffer));
+//        error_if(recv(clnt_sockfd, buffer, sizeof(buffer), 0)<=0, "Recv failed!!!");
         if(read_bytes > 0){
-            printf("message from client fd %d: %s\n", clnt_sockfd, buf);
-            write(clnt_sockfd, buf, sizeof(buf));
+            printf("Message from client fd %d: %s\n", clnt_sockfd, buffer);
+            write(clnt_sockfd, buffer, sizeof(buffer));
         } else if(read_bytes == 0){
             printf("client fd %d disconnected\n", clnt_sockfd);
             close(clnt_sockfd);
             break;
         } else if(read_bytes == -1){
             close(clnt_sockfd);
-            errif(true, "socket read error");
+            error_if(true, "socket read error");
         }
+        //bzero(buffer, sizeof(buffer));
+        //scanf("%s", buffer);
+        //ssize_t w = write(clnt_sockfd, buffer, sizeof(buffer));
+       	//error_if(send(clnt_sockfd, buffer, sizeof(buffer), 0)<=0, "Send failed!!!");
+        //if(w==-1){
+        //    printf("socket already disconnected, can't write any more!\n");
+        //    break;
+        //}
+//        printf("Recv: %s\n", buffer);
+//        sprintf(buffer, "Ip is %s\n", inet_ntoa(clnt_addr.sin_addr));
+//        error_if((send(clnt_sockfd, buffer, sizeof(buffer), 0)<=0),"Send failed!!!");
+//        printf("Send: %s\n", buffer);
+
     }
+    close(clnt_sockfd);
     close(sockfd);
     return 0;
 }
