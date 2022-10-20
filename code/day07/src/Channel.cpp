591f1e16d18da2a7da0e4c6956863a0a28b4d91a
Yap Miracle <2049829115@qq.com>
2022-10-20 15:24:24 +0800

    自己的版本

--- code/day07/src/Channel.cpp
+++ code/day07/src/Channel.cpp
@@ -1,50 +1,60 @@
+//
+// Created by mirac on 2022/10/11.
+//
+
 #include "Channel.h"
 #include "EventLoop.h"
 
-Channel::Channel(EventLoop *_loop, int _fd) : loop(_loop), fd(_fd), events(0), revents(0), inEpoll(false){
+Channel::Channel(EventLoop* _loop, int _fd): loop(_loop), fd(_fd), events(0), revents(0), inEpoll(false) {
 
 }
 
-Channel::~Channel()
-{
+Channel::~Channel() {
+
 }
 
-void Channel::handleEvent(){
+// 回调函数
+void Channel::handleEvent() {
     callback();
 }
 
-void Channel::enableReading(){
-    events |= EPOLLIN | EPOLLET;
+void Channel::enableReading() {
+    events = EPOLLIN | EPOLLET;
     loop->updateChannel(this);
 }
 
-int Channel::getFd(){
+int Channel::getFd() const {
     return fd;
 }
 
-uint32_t Channel::getEvents(){
+uint32_t Channel::getEvents() const {
     return events;
 }
-uint32_t Channel::getRevents(){
+
+//void Channel::setEvents(uint32_t events) {
+//    Channel::events = events;
+//}
+
+uint32_t Channel::getRevents() const {
     return revents;
 }
 
-bool Channel::getInEpoll(){
+void Channel::setRevents(uint32_t revents) {
+    Channel::revents = revents;
+}
+
+bool Channel::isInEpoll() const {
     return inEpoll;
 }
 
-void Channel::setInEpoll(){
+void Channel::setInEpoll() {
     inEpoll = true;
 }
 
-// void Channel::setEvents(uint32_t _ev){
-//     events = _ev;
-// }
-
-void Channel::setRevents(uint32_t _ev){
-    revents = _ev;
-}
-
+/**
+ * 设置回调函数
+ * @param _cb
+ */
 void Channel::setCallback(std::function<void()> _cb){
     callback = _cb;
 }
