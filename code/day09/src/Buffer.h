d73cccb6e7340a9591b80a4aabaf54b6c24ec1b8
Yap Miracle <2049829115@qq.com>
2022-10-24 16:18:02 +0800

    添加Buffer类

--- code/day09/src/Buffer.h
+++ code/day09/src/Buffer.h
@@ -10,7 +10,7 @@ public:
     ~Buffer();
     void append(const char* _str, int _size);
     ssize_t size();
-    const char* c_str();
+     char* c_str() const;
     void clear();
     void getline();
 };
